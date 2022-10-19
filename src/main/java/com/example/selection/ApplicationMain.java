8fd4a9e4b9ec1f7d72c556b321ca9976071601b4
Henry Jobst <github@jobst-berlin.de>
2022-10-19 22:43:16 +0200

    change application name

--- src/main/java/com/example/selection/ApplicationMain.java
+++ src/main/java/com/example/selection/ApplicationMain.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParameterTestApplication {
+public class ApplicationMain {
     public static void main(String[] args) {
-        SpringApplication.run(ParameterTestApplication.class, args);
+        SpringApplication.run(ApplicationMain.class, args);
     }
 }
